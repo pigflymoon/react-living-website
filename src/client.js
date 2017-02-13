e3d471d119089b5b819ca8a9ad4b99470f36166b
pigflymoon <pplflymoon2015@gmail.com>
2017-02-13 22:40:27 +1300

    #babel for jsx and css

--- src/client.js
+++ src/client.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+
+import App from '../components/App';
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,35 @@ const webpack = require('webpack')
 const path = require('path')
 
 const config = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './client.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'src'),
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['es2015', { modules: false }]
-          ]
-        }
-      }]
-    }]
-  }
+    context: path.resolve(__dirname, 'src'),
+    entry: './client.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['es2015', {modules: false}]
+                    ]
+                }
+            }]
+        },
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel-loader?presets[]=es2015,presets[]=react'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader?modules"
+            },]
+    }
 }
 
 module.exports = config

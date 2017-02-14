5e11a64ca882ffcb03c1098f40aeba5e481bf78b
pigflymoon <pplflymoon2015@gmail.com>
2017-02-14 16:22:45 +1300

    #website frame

--- components/Layout.js
+++ components/Layout.js
@@ -1,94 +1,28 @@
 import React from 'react';
 import App from '../components/App';
+import Gallery from '../components/Gallery';
 import style from '../components/Layout.css';
-import {Flex, Box} from 'reflexbox';
-import {Avatar} from 'rebass';
-import {CardImage} from 'rebass';
+
 
 export default () => {
     return (
         <div>
-            <App />
-            <Flex
-                align="center"
-                justify="space-between"
-            >
-                <Box className={style.Box}
-                     auto
-                     p={3}
-                >
-                    Box auto
-                    <Avatar
-                        circle
-                        size={48}
-                        src="http://lorempixel.com/64/64/cats"
-                    />
-                </Box>
-                <Box className={style.Box} p={3}>
-                    Box
-                </Box>
-            </Flex>
-
-            <Flex wrap>
-                <Box
-                    col={12}
-                    lg={3}
-                    sm={6}
-                >
-                    <div
-                        style={{maxWidth: 192}}>
-                        <CardImage src="http://placehold.it/320/08e/fff"/>
-                    </div>
-                </Box>
-                <Box
-                    col={12}
-                    lg={3}
-                    sm={6}
-                >
-                    <div
-                        style={{maxWidth: 192}}>
-                        <CardImage src="http://placehold.it/320/08e/fff"/>
-                    </div>
-                </Box>
-                <Box
-                    col={12}
-                    lg={3}
-                    sm={6}
-                >
-                    <div
-                        style={{maxWidth: 192}}>
-                        <CardImage src="http://placehold.it/320/08e/fff"/>
-                    </div>
-                </Box>
-                <Box
-                    col={12}
-                    lg={3}
-                    sm={6}
-                >
-                    <div
-                        style={{maxWidth: 192}}>
-                        <CardImage src="http://placehold.it/320/08e/fff"/>
-                    </div>
-                </Box>
-            </Flex>
-            <Flex>
-                <div
-                    style={{maxWidth: 192}}>
-                    <CardImage src="http://placehold.it/320/08e/fff"/>
-                </div>
-                <div
-                    style={{maxWidth: 192}}>
-                    <CardImage src="http://placehold.it/320/08e/fff"/>
-                </div>
-                <div
-                    style={{maxWidth: 192}}>
-                    <CardImage src="http://placehold.it/320/08e/fff"/>
-                </div>
-                <div
-                    style={{maxWidth: 192}}>
-                    <CardImage src="http://placehold.it/320/08e/fff"/>
-                </div>
-            </Flex>
+            <header>
+                <ul className={style.navigation}>
+                    <li><a>Home</a></li>
+                    <li><a>Articles</a></li>
+                    <li><a>Solutions</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </header>
+            <div className={style.container}>
+                <section className={style.main}>
+                    <Gallery />
+                </section>
+                <aside className={style.sidebar}></aside>
+            </div>
+            <footer></footer>
         </div>
     );
 };

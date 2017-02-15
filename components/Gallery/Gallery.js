d2da390c13fc47365f35570aed97b2b60fcee3ef
pigflymoon <pplflymoon2015@gmail.com>
2017-02-15 14:26:57 +1300

    #export Gallery

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Gallery.scss';
 
-export default () => {
-    return (
-        <div className="gallery">
-            <div className="item">1</div>
-            <div className="item">1</div>
-            <div className="item">1</div>
-            <div className="item">1</div>
-        </div>
-    );
-};
+export default class Gallery extends React.Component {
+    render() {
+
+        return (
+            <div className="gallery">
+                <div className="item">1</div>
+                <div className="item">1</div>
+                <div className="item">1</div>
+                <div className="item">1</div>
+            </div>
+        );
+    };
+}
 

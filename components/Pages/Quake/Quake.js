3da95582b304b7852fca59f6ac8d37615f0d875f
pigflymoon <pplflymoon2015@gmail.com>
2017-03-01 16:28:36 +1300

    # make progress

--- components/Pages/Quake/Quake.js
+++ components/Pages/Quake/Quake.js
@@ -5,21 +5,10 @@ import './Quake.scss';
 
 
 class QuakeMap extends React.Component {
-    state = {
-        forecast: {}
-    };
-
-    propTypes = {
-        np_url: React.PropTypes.string,
-        init_lat: React.PropTypes.number,
-        init_lng: React.PropTypes.number
-    };
-
     markers = [];
 
-
     render() {
-        return <div className="NpsForecastMap" ref="mapdiv">
+        return <div className="quake-map" ref="mapdiv">
         </div>
     }
 
@@ -30,7 +19,7 @@ class QuakeMap extends React.Component {
 
     createMap() {
         let mapOptions = {
-            zoom: 3,
+            zoom: 6,
             center: this.mapCenter()
         }
         return new google.maps.Map(this.refs.mapdiv, mapOptions)
@@ -49,39 +38,17 @@ class QuakeMap extends React.Component {
         axios.get(self.props.nps_source)
             .then(function (result) {
                 for (let val of result.data.features) {
-                    let marker = self.createMarker(val, self.map)
+                    let marker = self.createMarker(val, self.map)//self.map is  this.map = this.createMap() pass created map into here
 
                     marker.addListener('click', function () {
                         infoWindow.close()
-                        let title = this.locality
-                        let infoContent = ""
-                        let getWeather = false
-
-
-                        // check to see if we already have the forecast for this marker
-                        if (title in self.state.forecast) {
-                            console.log("title in state")
-                            infoContent = title + "<br>" + self.state.forecast[title]
-                        }
-                        else {
-                            infoContent = title + "<br>Loading Current Weather..."
-                            getWeather = true
-                        }
+                        let title = this.title
+                        let infoContent = title
                         infoWindow.setContent(infoContent)
                         infoWindow.open(self.map, this)
-
-                        // if (getWeather) {
-                        //     self.getForecast(marker.position.lat(), marker.position.lng())
-                        //         .then(function (result) {
-                        //             infoWindow.setContent(title + "<br>" + result)
-                        //             self.state.forecast[title] = result
-                        //             console.log("added forecast to state for " + title)
-                        //         })
-                        // }
-
                     });
 
-                    self.markers.push(marker)
+                    self.markers.push(marker)//add markers as a property in self
 
                 } // for
             }); //then
@@ -91,7 +58,10 @@ class QuakeMap extends React.Component {
         let pointval = new google.maps.LatLng(
             parseFloat(val['geometry']['coordinates'][1]),
             parseFloat(val['geometry']['coordinates'][0]));
-        let titleText = val['properties']['locality']
+        let locality = val['properties']['locality']
+        let time = val['properties']['time']
+        time = time.split(".")[0].replace(/-/g, '/').replace(/T/g, '  ')
+        let titleText = locality + '<br/>' + time
         let marker = new google.maps.Marker({
             position: pointval,
             map: map,
@@ -101,39 +71,23 @@ class QuakeMap extends React.Component {
         return marker
     }
 
-    getForecast(lat, lng) {
-        let forecast_p = new Promise(
-            function (resolve, reject) {
-                let request_url = "https://crossorigin.me/https://api.darksky.net/forecast/8266ff95ef9bbfccf0ea24c325818f31/"
-
-                request_url = request_url + lat + "," + lng
-                axios.get(request_url)
-                    .then(function (result) {
-                        let content = result.data.daily.summary
-                        resolve(content)
-                    });
-            }); // function(success,fail)
-
-        forecast_p.then(
-            function (result) {
-                return result
-            }
-        );
-        return forecast_p
-    }
-
 }
 
-let init_lng = 174.470;
-let init_lat = 41.180;
+QuakeMap.propTypes = {
+    np_url: React.PropTypes.string,
+    init_lat: React.PropTypes.number,
+    init_lng: React.PropTypes.number
+};
+
+const init_lng = 174.885971;
+const init_lat = -40.900557;
 let nps_url = "https://api.geonet.org.nz/quake?MMI=3";
 
 export default class Quake extends React.Component {
     render() {
         return (
             <div className="bg-white">
-                <QuakeMap init_lat={init_lat} init_lng={init_lng} nps_source={nps_url}/>,
-
+                <QuakeMap init_lat={init_lat} init_lng={init_lng} nps_source={nps_url}/>
             </div>
         )
     }
